2458bfe171c11581d5fa01c42b630304d7aad5f0
Elena Safina <elena.shxx@gmail.com>
2022-09-04 10:41:32 +0300

    request error loggers

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
   console.log(`App listening on port ${PORT}`);
 });
+
+app.use(requestLogger); // подключение логгера запросов
+app.use(errorLogger); // подключение логгера ошибок
