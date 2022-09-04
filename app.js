2947aef0c1089053ed5d3e93e0181863260322ff
Elena Safina <elena.shxx@gmail.com>
2022-09-04 17:31:44 +0300

    add use routers

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const { errors } = require('celebrate');
 const express = require('express');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const routerUsers = require('./routes/users');
+// const routerMovies = require('./routes/movies');
+
+const NotFoundError = require('./errors/NotFoundError');
+
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -12,6 +17,15 @@ app.listen(PORT, () => {
 });
 
 app.use(requestLogger); // подключение логгера запросов
+
+// роуты, которым авторизация нужна
+app.use('/users', routerUsers);
+// app.use('/movies', routerMovies);
+
+// несуществующие роуты
+app.use('*', () => {
+  throw new NotFoundError({ message: 'Страницы не существует' });
+});
 
 app.use(errorLogger); // подключение логгера ошибок
 app.use(errors());
