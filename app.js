396fbeb90ce3ee7acf985e928d938277f7d9f4da
Elena Safina <elena.shxx@gmail.com>
2022-09-07 09:24:51 +0300

    cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { errors } = require('celebrate');
 const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const { limiter } = require('./utils/limiter');
 
 const handleErrors = require('./errors/handleErrors');
@@ -31,35 +32,7 @@ app.use(helmet());
 app.use(limiter);
 app.use(express.json());
 
-// Массив доменов, с которых разрешены кросс-доменные запросы
-const allowedCors = [
-  'https://polumnana.movies.nomorepartiesxyz.ru',
-  'http://polumnana.movies.nomorepartiesxyz.ru',
-  'localhost:3000',
-  'localhost:3002',
-];
-
-// eslint-disable-next-line consistent-return
-app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-
-  next();
-});
-
+app.use(cors());
 app.use(router);
 
 // подключение логгера ошибок
